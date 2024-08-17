0f220cbda16e51bf19dc27aed69d70ebc35e6d56
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-17 21:47:35 +0100

    Sidebar styling

--- 0.3GeminiAI/src/Components/SideBar/SideBar.jsx
+++ 0.3GeminiAI/src/Components/SideBar/SideBar.jsx
@@ -5,39 +5,48 @@ import { assets } from '../../assets/assets';
 
 const SideBar = () => {
 
-  return (
-    <div className='sideBar'>
-        <div className="top">
-            <img className='menu' src={assets.barsSolid} alt="" />
-            <div className="newChat">
-                <img src={assets.plusSolid} alt="" />
-                <p>New chat</p>
+    const [extended, setExtended] = useState(false);
+
+    const setMenubar = () => {
+        setExtended(!extended);
+    }
+
+    return (
+
+        <div className='sideBar'>
+            <div className="top">
+                <img onClick={setMenubar} className='menu' src={assets.barsSolid} alt="" />
+                <div className="newChat">
+                    <img src={assets.plusSolid} alt="" />
+                    {extended ? <p>New chat</p> : null}
+                </div>
+                {extended ?
+                    <div className="recent">
+                        <p className="recentTitle">Recent</p>
+                        <div className="recentEntry">
+                            <img src={assets.message} alt="" />
+                            <p>What is Nodejs ..</p>
+                        </div>
+                    </div>
+                    : null}
             </div>
-            <div className="recent">
-                <p className="recentTitle">Recent</p>
-                <div className="recentEntry">
-                    <img src={assets.message} alt="" />
-                    <p>What is Nodejs ..</p>
+
+            <div className="bottom">
+                <div className="bottomItem recentEntry">
+                    <img src={assets.circleQuestion} alt="" />
+                    {extended ? <p>Help</p> : null}
+                </div>
+                <div className="bottomItem recentEntry">
+                    <img src={assets.clockRotateLeft} alt="" />
+                    {extended ? <p>History</p> : null}
+                </div>
+                <div className="bottomItem recentEntry">
+                    <img src={assets.gearSolid} alt="" />
+                    {extended ? <p>Settings</p> : null}
                 </div>
             </div>
         </div>
-
-        <div className="bottom">
-            <div className="bottomItem recentEntry">
-                <img src={assets.circleQuestion} alt="" />
-                <p>Help</p>
-            </div>
-            <div className="bottomItem recentEntry">
-                <img src={assets.clockRotateLeft} alt="" />
-                <p>History</p>
-            </div>
-            <div className="bottomItem recentEntry">
-                <img src={assets.gearSolid} alt="" />
-                <p>Settings</p>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default SideBar;
