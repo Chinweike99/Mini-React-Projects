f2575506c395b0619649fc31258d66183b574ec0
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-17 07:59:26 +0100

    updated

--- 0.2_ImageGenerator/image_generator/src/Components/ImageGenerator/ImageGenerator.jsx
+++ 0.2_ImageGenerator/image_generator/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -7,7 +7,25 @@ function ImageGenerator() {
     const [imgUrl, setImageUrl] = useState("/");
     let inputRef = useRef(null);
 
-    
+    const generateImage = async() => {
+        if(inputRef.current.value === "") {
+            return 0;
+        }
+        const response = await fetch("https://api.openai.com/v1/images/generations",{
+            method: "POST",
+            headers: {"Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.API_IMAGEKEY}`,
+                "User-Agent": "Chrome",
+            },
+            body: JSON.stringify({
+                prompt: `${inputRef.current.value}`,
+                n:1,
+                size:"512x512"
+            }),
+        });
+        let data = await response.json();
+        console.log(data);
+    }
 
   return (
     <div className="ai-image-generator">
@@ -21,7 +39,7 @@ function ImageGenerator() {
         </div>
         <div className="search-box">
             <input type="text" ref={inputRef} className='search-input' placeholder='Describe Image you want'/>
-            <div className="generate-btn">Generate</div>
+            <div className="generate-btn" onClick={() => generateImage()}>Generate</div>
         </div>
     </div>
   );
