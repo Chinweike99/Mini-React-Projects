1b9b0eb9b5350b79b1fbedde555e1f3406b66136
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-17 15:42:12 +0100

    Icons

---
+++ 0.3GeminiAI/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import imagesSolid from './assets/imagesSolid.svg'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <img src={imagesSolid} alt="" />
+    </div>
+  )
+}
+
+export default App
