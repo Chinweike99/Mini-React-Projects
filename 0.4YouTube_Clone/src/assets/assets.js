5fd403111228d357e6667796da5af4d90eaf08eb
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-26 10:10:19 +0100

    assets

---
+++ 0.4YouTube_Clone/src/assets/assets.js
@@ -0,0 +1,25 @@
+import bellSolid from './bellSolid.svg'
+import glassSolid from './glassSolid.svg'
+import houseSolid from './houseSolid.svg'
+import menuBar from './menuBar.svg'
+import microphoneSolid from './microphoneSolid.svg'
+import tvSolid from './tvSolid.svg'
+import videoSolid from './videoSolid.svg'
+import youTube from './youTube.jpeg'
+import uploadSolid from './uploadSolid.svg'
+import userRegular from './userRegular'
+
+const assets = {
+    bellSolid,
+    glassSolid,
+    houseSolid,
+    menuBar,
+    microphoneSolid,
+    tvSolid,
+    videoSolid,
+    youTube,
+    uploadSolid,
+    userRegular
+}
+
+export default assets;
