e342d0107fa9ff0aae63daa60731849f2ac49e2e
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-27 16:15:19 +0100

    Images

--- 0.4YouTube_Clone/src/assets/assets.js
+++ 0.4YouTube_Clone/src/assets/assets.js
@@ -24,8 +24,27 @@ import image4 from './image4.jpg'
 import image6 from './image6.jpg'
 import images1 from './images1.jpg'
 import images3 from './images3.jpg'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+
 
 const assets = {
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
     amandla,
     image2,
     image4,
