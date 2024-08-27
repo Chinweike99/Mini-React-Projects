e6efff70453c7604c6cb28e3991c8e2d3c170fc1
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-27 07:50:29 +0100

    Images

--- 0.4YouTube_Clone/src/assets/assets.js
+++ 0.4YouTube_Clone/src/assets/assets.js
@@ -9,6 +9,15 @@ import youTube from './youTube.png'
 import uploadSolid from './uploadSolid.svg'
 import userRegular from './userRegular.svg'
 import puzzleSolid from './puzzleSolid.svg'
+import blogSolid from './blogSolid.svg'
+import carSolid from './carSolid.svg'
+import gamepadSolid from './gamepadSolid.svg'
+import hackerrankSolid from './hackerrankSolid.svg'
+import microchipSolid from './microchipSolid.svg'
+import musicSolid from './musicSolid.svg'
+import newsSolid from './newsSolid.svg'
+import playstationSolid from './playstationSolid.svg'
+import sport from './sport.svg'
 
 const assets = {
     bellSolid,
@@ -21,7 +30,16 @@ const assets = {
     youTube,
     uploadSolid,
     userRegular,
-    puzzleSolid
+    puzzleSolid,
+    blogSolid,
+    carSolid,
+    gamepadSolid,
+    hackerrankSolid,
+    microchipSolid,
+    musicSolid,
+    newsSolid,
+    playstationSolid,
+    sport
 }
 
 export default assets;
