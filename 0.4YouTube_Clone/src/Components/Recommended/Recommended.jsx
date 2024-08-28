10b6b73f75f7ab15de7e56c1c2240b1b70d5cc42
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-28 17:05:19 +0100

    Recommended videos components

---
+++ 0.4YouTube_Clone/src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Recommended.css'
+
+const Recommended = () => {
+    return(
+        <div>
+            Hello Boss
+        </div>
+    )
+}
+
+export default Recommended;
--- 0.4YouTube_Clone/src/Pages/Video/Video.jsx
+++ 0.4YouTube_Clone/src/Pages/Video/Video.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/Playvideo/Playvideo';
+import Recommended from '../../Components/Recommended/Recommended';
 
 const Video = () => {
   return (
     <div className='playContainer'>
       <PlayVideo />
+      <Recommended />
     </div>
   )
 }
