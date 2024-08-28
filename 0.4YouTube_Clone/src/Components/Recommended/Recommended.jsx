d6c3d476bd1584a6c37f54dfb8278a528e9deca0
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-28 18:30:29 +0100

    Updated

--- 0.4YouTube_Clone/src/Components/Recommended/Recommended.jsx
+++ 0.4YouTube_Clone/src/Components/Recommended/Recommended.jsx
@@ -1,10 +1,74 @@
 import React from "react";
 import './Recommended.css'
+import assets from "../../assets/assets";
 
 const Recommended = () => {
     return(
-        <div>
-            Hello Boss
+        <div className="recommended">
+            <div className="sideVideos">
+                <img src={assets.Ai} alt="" />
+                <div className="videoInfo">
+                    <h4>Best Channel to learn about Artificial Intelligence</h4>
+                    <p>AiTutor</p>
+                    <p>400k views</p>
+                </div>
+            </div>
+            <div className="sideVideos">
+                <img src={assets.dataAnalysis2} alt="" />
+                <div className="videoInfo">
+                    <h4>Best Channel to learn about Artificial Intelligence</h4>
+                    <p>AiTutor</p>
+                    <p>400k views</p>
+                </div>
+            </div>
+            <div className="sideVideos">
+                <img src={assets.comedy} alt="" />
+                <div className="videoInfo">
+                    <h4>Best Channel to learn about Artificial Intelligence</h4>
+                    <p>AiTutor</p>
+                    <p>400k views</p>
+                </div>
+            </div>
+            <div className="sideVideos">
+                <img src={assets.dataAnalysis} alt="" />
+                <div className="videoInfo">
+                    <h4>Best Channel to learn about Artificial Intelligence</h4>
+                    <p>AiTutor</p>
+                    <p>400k views</p>
+                </div>
+            </div>
+            <div className="sideVideos">
+                <img src={assets.Ai2} alt="" />
+                <div className="videoInfo">
+                    <h4>Best Channel to learn about Artificial Intelligence</h4>
+                    <p>AiTutor</p>
+                    <p>400k views</p>
+                </div>
+            </div>
+            <div className="sideVideos">
+                <img src={assets.health} alt="" />
+                <div className="videoInfo">
+                    <h4>Best Channel to learn about Artificial Intelligence</h4>
+                    <p>AiTutor</p>
+                    <p>400k views</p>
+                </div>
+            </div>
+            <div className="sideVideos">
+                <img src={assets.amandla} alt="" />
+                <div className="videoInfo">
+                    <h4>Best Channel to learn about Artificial Intelligence</h4>
+                    <p>AiTutor</p>
+                    <p>400k views</p>
+                </div>
+            </div>
+            <div className="sideVideos">
+                <img src={assets.health2} alt="" />
+                <div className="videoInfo">
+                    <h4>Best Channel to learn about Artificial Intelligence</h4>
+                    <p>AiTutor</p>
+                    <p>400k views</p>
+                </div>
+            </div>
         </div>
     )
 }
