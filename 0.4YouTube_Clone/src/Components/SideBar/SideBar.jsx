00bf12240bf6b2aa849d763e81e80a2bc49c49a6
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-28 19:01:42 +0100

    YoutubeAPi

--- 0.4YouTube_Clone/src/Components/SideBar/SideBar.jsx
+++ 0.4YouTube_Clone/src/Components/SideBar/SideBar.jsx
@@ -2,35 +2,35 @@ import { useState } from 'react'
 import './SideBar.css'
 import assets from '../../assets/assets';
 
-const SideBar = ({sidebar}) => {
+const SideBar = ({sidebar, category, setCategory}) => {
   return (
     <div className={`sideBar ${sidebar?"" : "smallSideBar"}`}>
       <div className="shortLink">
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===0?"active" : ""}`} onClick={() => setCategory(0)}>
           <img src={assets.houseSolid} alt="" /><p>Home</p>
         </div>
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===20?"active" : ""}`} onClick={() => setCategory(20)}>
           <img src={assets.gamepadSolid} alt="" /><p>Gaming</p>
         </div>
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===2?"active" : ""}`} onClick={() => setCategory(2)}>
           <img src={assets.carSolid} alt="" /><p>Automobiles</p>
         </div>
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===17?"active" : ""}`} onClick={() => setCategory(17)}>
           <img src={assets.sport} alt="" /><p>Sport</p>
         </div>
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===24?"active" : ""}`} onClick={() => setCategory(24)}>
           <img src={assets.playstationSolid} alt="" /><p>Entertainment</p>
         </div>
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===28?"active" : ""}`} onClick={() => setCategory(28)}>
           <img src={assets.microchipSolid} alt="" /><p>Technology</p>
         </div>
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===10?"active" : ""}`} onClick={() => setCategory(10)}>
           <img src={assets.musicSolid} alt="" /><p>Music</p>
         </div>
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===22?"active" : ""}`} onClick={() => setCategory(22)}>
           <img src={assets.blogSolid} alt="" /><p>Blogs</p>
         </div>
-        <div className="sideLinks">
+        <div className={`sideLinks ${category===25?"active" : ""}`} onClick={() => setCategory(25)}>
           <img src={assets.newsSolid} alt="" /><p>News</p>
         </div>
         <hr />
--- 0.4YouTube_Clone/src/Pages/Home/Home.jsx
+++ 0.4YouTube_Clone/src/Pages/Home/Home.jsx
@@ -4,11 +4,12 @@ import SideBar from '../../Components/SideBar/SideBar'
 import Feed from '../../Components/Feed/Feed'
 
 const Home = ({sidebar}) => {
+  const [category, setCategory] = useState(0);
   return (
     <>
-      <SideBar sidebar={sidebar}/>
+      <SideBar sidebar={sidebar} category={category} setCategory={setCategory}/>
       <div className={`container ${sidebar? "" : "largeContainer"}`}>
-          <Feed />
+          <Feed category={category}/>
       </div>
       
     </>
