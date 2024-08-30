d7b07efaf98f02670e1a89c7cd4fa6ebaf2dec2a
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-30 19:24:41 +0100

    updated

--- 0.4YouTube_Clone/src/Components/Playvideo/Playvideo.jsx
+++ 0.4YouTube_Clone/src/Components/Playvideo/Playvideo.jsx
@@ -1,17 +1,56 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './Playvideo.css'
 import assets from "../../assets/assets";
+import { valueConverter } from "../../data";
+import moment from "moment";
 
 const PlayVideo = ({videoId}) =>{
+
+    const [apiData, setApiData] = useState(null);
+    const [channelData, setChannelData] = useState(null)
+    const [channelComment, setChannelComment] = ([]); 
+
+    const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+    // FETCH VIDEO DATA
+    const fetchVideoData = async ()=>{
+        const videoDetails = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+
+        await fetch(videoDetails).then(res=>res.json()).then(data=>setApiData(data.items[0]))
+    }
+
+    // FETCH CHANNEL DATA
+    const fetchChannelData = async() => {
+        const channelDetails =`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
+
+        await fetch(channelDetails).then(res=>res.json()).then(data=>setChannelData(data.items[0]))
+    }
+
+    // FECTCH REAL COMMENTS
+    const fetchCommentData = async()=>{
+        const commentDetails = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=${videoId}&key=${API_KEY}`;
+
+        await fetch(commentDetails).then(res=>res.json()).then(data=>setChannelComment(data.items))
+    }
+
+
+
+    useEffect(() => {
+        fetchVideoData();
+    }, [])
+
+    useEffect(() => {
+        fetchChannelData();
+    }, [apiData])
+
     return (
         <div className="playVideo">
             {/* <video src={assets.video1} controls autoPlay muted></video> */}
             <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            <h3>Learning web development</h3>
+            <h3>{apiData?apiData.snippet.title:"Write a title"}</h3>
             <div className="playVideoInfo">
-                <p>2918 views &bull; 3 days ago</p>
+                <p>{valueConverter(apiData?apiData.statistics.viewCount: "0")}views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow(): ""}</p>
                 <div>
-                    <span><img src={assets.thumbsUp} alt="" /> 190</span>
+                    <span><img src={assets.thumbsUp} alt="" />{valueConverter(apiData?apiData.statistics.likeCount: 0)}</span>
                     <span><img src={assets.thumbsDown} alt="" />1</span>
                     <span><img src={assets.shareSolid} alt="" />Share</span>
                     <span><img src={assets.downloadSolid} alt="" />Save</span>
@@ -19,18 +58,19 @@ const PlayVideo = ({videoId}) =>{
             </div>
             <hr />
             <div className="publisher">
-                <img src={assets.amandla} alt="" />
+                <img src={channelData?channelData.snippet.thumbnails.default.url : ""} alt="" />
                 <div>
-                    <p>Chinweike</p>
-                    <span>2M Subscribers</span>
+                    <p>{apiData?apiData.snippet.channelTitle : ""}</p>
+                    <span>{valueConverter(channelData?channelData.statistics.subscriberCount : "")} Subscribers</span>
                 </div>
                 <button>Subscribe</button>
             </div>
             <div className="videoDiscription">
-                <p>Learn to code with ease.</p>
-                <p>Subscribe to get notified on uplaod of latest videos.</p>
+            <p >{apiData?apiData.snippet.description.slice(0,250):"Write a description"}</p>
+                {/* <p>Learn to code with ease.</p>
+                <p>Subscribe to get notified on uplaod of latest videos.</p> */}
                 <hr />
-                <h4>350 Comments</h4>
+                <h4>{valueConverter(apiData?apiData.statistics.commentCount: "0")} Comments</h4>
                 <div className="comment">
                     <img src={assets.userRegular} alt="" />
                     <div>
