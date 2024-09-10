3a2235eb622a746cca1562e08f909dd473e6e2eb
Chinweike99 <innocentakwolu1@gmail.com>
2024-09-10 10:06:05 +0100

    updated stories

--- 0.5chat_app/src/Components/Stories/Stories.jsx
+++ 0.5chat_app/src/Components/Stories/Stories.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import './Stories.css'
+import './Stories.scss'
+import assets from '../../assets/assets'
 
 export const Stories = () => {
+
+    const stories = [
+        {id: 1, name: "John Doe", img: `${assets.image1}` },
+        {id: 2, name: "Amandla Sternberg", img: `${assets.image6}` },
+        {id: 3, name: "David Adeleke", img: `${assets.image4}` },
+        {id: 4, name: "John Doe", img: `${assets.images1}` },
+        {id: 5, name: "Stephanie Mathias", img: `${assets.img1}` },
+    ];
+
+
   return (
     <div>
-        This is my Story
+        <div className="stories">
+            {stories.map((item, index)=>{
+                return(
+                    <div className='myStory' key={index}>
+                        <img src={item.img} alt="" />
+                        <p>{item.name}</p>
+                    </div>
+                )
+            })}
+        </div>
     </div>
   )
 }
