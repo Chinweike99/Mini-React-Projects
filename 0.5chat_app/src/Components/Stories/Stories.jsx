dcb284641bb2efb3c0de44da41f0c0941bb739d2
Chinweike99 <innocentakwolu1@gmail.com>
2024-09-10 11:16:44 +0100

    Stories

--- 0.5chat_app/src/Components/Stories/Stories.jsx
+++ 0.5chat_app/src/Components/Stories/Stories.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Stories.scss'
 import assets from '../../assets/assets'
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import { AuthContext } from '../../Context/authContext';
 
 export const Stories = () => {
+    const {currentUser} = useContext(AuthContext);
 
     const stories = [
         {id: 1, name: "John Doe", img: `${assets.image1}` },
-        {id: 2, name: "Amandla Sternberg", img: `${assets.image6}` },
-        {id: 3, name: "David Adeleke", img: `${assets.image4}` },
+        {id: 2, name: "Amandla Sternberg", img: `${assets.image4}` },
+        {id: 3, name: "David Adeleke", img: `${assets.image6}` },
         {id: 4, name: "John Doe", img: `${assets.images1}` },
         {id: 5, name: "Stephanie Mathias", img: `${assets.img1}` },
     ];
@@ -16,6 +19,15 @@ export const Stories = () => {
   return (
     <div>
         <div className="stories">
+
+            <div className='myStory'>
+                <img src={currentUser.profilePic} alt="" />
+                <p>{currentUser.name}</p>
+            </div>
+
+            <div className="cross">
+                <AddOutlinedIcon />
+            </div>
             {stories.map((item, index)=>{
                 return(
                     <div className='myStory' key={index}>
