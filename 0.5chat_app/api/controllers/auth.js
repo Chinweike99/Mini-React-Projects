a7bfef0363c2c68e564d46f66426627140288184
Chinweike99 <innocentakwolu1@gmail.com>
2024-09-16 12:05:40 +0100

    Middlewares

--- 0.5chat_app/api/controllers/auth.js
+++ 0.5chat_app/api/controllers/auth.js
@@ -15,6 +15,7 @@ export const register = (req, res) => {
             return res.status(500).json(err);
         }
         if (data.rows.length) { // Check if the user already exists
+            console.log("user exists")
             return res.status(409).json("User already exists");
         }
 
@@ -30,6 +31,7 @@ export const register = (req, res) => {
                 console.log(err);
                 return res.status(500).json(err);
             }
+            console.log("user created")
             return res.status(200).json("User has been created");
         });
     });
@@ -38,7 +40,23 @@ export const register = (req, res) => {
 
 
 // LOGIN PAGE
-export const login = () => {
+export const login = (req, res) => {
+
+    const q = "SELECT * FROM account_user WHERE username = $1";
+
+    db.query(q, [req.body.username], (err, data) => {
+        if(err){
+            return res.status(500).json(err)
+        }
+        if(data.rows.length === 0){
+            return res.status(404).json("User not found")
+        }
+
+        const checkPassword = bcrypt.compareSync(req.body.password, data[0].password);
+
+        if(!checkPassword) return res.status(400).json("Wrong password or Username")
+
+    })
 
 }
 
--- 0.5chat_app/api/index.js
+++ 0.5chat_app/api/index.js
@@ -7,12 +7,16 @@ import postRoutes from "./Routes/posts.js";
 import commentsRoutes from "./Routes/comments.js";
 import authRoutes from "./Routes/auth.js";
 import likesRoutes from "./Routes/likes.js";
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 const app = express();
 
 const port = 3200;
 
 // Middlewares: Without middlewares sedning json objects won't be possible
 app.use(express.json())
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
